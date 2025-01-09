02c5cee7d54b35c3dd7c8967eab6bdd572c7d382
Alberto-ll <Lautaroalbertolier@gmail.com>
2025-01-09 09:18:37 -0300

    base de datos

--- proyecto/proyecto/models/model.py
+++ proyecto/proyecto/models/model.py
@@ -1,8 +1,27 @@
 import reflex as rx
-
+import  sqlmodel
+from typing import List, Optional
+"""
 class Direcciones(rx.Model, table=True):
     sector:str
     ip:str
     estado:bool
+"""
+
+# Nombre de la oficina
+class Oficinas(rx.Model, table=True):
+    nomber:str
     
-    
+# computadora asociada a una oficina
+class Computadora(rx.Model,table=True):
+    hostname:str
+    ip:str
+    estado:bool
+    oficina_id:int
+
+# impresora red de la oficina
+class Impresora(rx.Model,table=True):
+    hostname:str
+    ip:str
+    estado:str
+    oficina_id: int 
