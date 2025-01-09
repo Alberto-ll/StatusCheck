762bada251e48c5ab43247ce5e7443751317874e
Alberto-ll <Lautaroalbertolier@gmail.com>
2025-01-09 09:58:34 -0300

    frontend setup

--- proyecto/proyecto/proyecto.py
+++ proyecto/proyecto/proyecto.py
@@ -1,21 +1,5 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
-
+"""Paguina donde se cargaran todas las paguinas"""
 import reflex as rx
-from .components.form import formulario
-from .components.tabla import tabla
-from .services.ipManagment import Manejador
-
-from rxconfig import config
+from pages import dashboard, officeDisplay
     
-
-
-# Paguina inical 
-def main():
-    return rx.container(
-        formulario(),
-        tabla()
-    )
-    
-    
-app = rx.App()
-app.add_page(main, route="/",on_load=Manejador.estadoUpdate())
